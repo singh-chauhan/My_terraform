634964805bb36a438c3ba04826f6e34b8b1fd6b6
Chauhan <nishant.chauhan@dell.com>
2026-04-28 13:31:09 +0530

    clean repo

---
+++ vpc_creation/variables.tf
@@ -0,0 +1,36 @@
+variable "AWS_region" {
+    description = "AWS Region to deploy resources"
+    type = string
+    default = "ap-south-1"
+  
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for VPC"
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "subnet_newbits" {
+    description = "Number of new bits to add to VPC CIDR to generate new subnets (e.g: 8 means /24 as CIDR block /16+8)"
+    type = number
+    default = 8
+  
+}
+
+variable "tags" {
+    description = "Global tage to apply all resources"
+    type = map(string)
+    default = {
+      Terraform = "true"
+    }
+  
+}
+
+variable "environment_name" {
+    description ="Environment name used in resource names and tags"
+    type = string
+    default = "Dev"
+  
+}
